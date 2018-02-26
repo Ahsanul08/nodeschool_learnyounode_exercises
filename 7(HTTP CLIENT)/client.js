844f74e1f4dd147c48d8607fe1ee4092467bcb73
Ahsanul08 <ahsanul.haque.ovi@gmail.com>
2018-02-26 13:51:17 +0600

    Solution of task 7 Added.

---
+++ 7(HTTP CLIENT)/client.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function (response) {
+    response.on("error", function(err){
+        throw err;
+    });
+
+    response.on("data", function(data){
+        console.log(data.toString());
+    });
+});
